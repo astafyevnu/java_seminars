b45e3bb3b630038f1798a7a37aeee0b473f8d157
Nikolay <n.astafyev@yandex.ru>
2022-12-31 13:42:51 +0800

    add tasks

---
+++ seminar3/hw/m2_del_nums.java
@@ -0,0 +1,29 @@
+package seminar3.hw;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class m2_del_nums {
+    public static void main(String[] args) {
+
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            Random random = new Random();
+            int num = random.nextInt(100);
+            arr.add(num);
+        }
+
+        System.out.println("\nИсходный список рандомных целых чисел: \n" + arr);
+
+        for (int i = 0; i < arr.size();) {
+            if (arr.get(i) % 2 == 0) {
+                arr.remove(i);
+            } else {
+                i++;
+            }
+        }
+        System.out.println("Список после удаления четных чисел:\n" + arr);
+
+    }
+}
