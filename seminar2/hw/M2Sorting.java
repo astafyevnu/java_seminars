76870a40d33190407cc2867be4caa0cb0ae68548
Nikolay <79745981+astafyevnu@users.noreply.github.com>
2023-01-03 20:49:20 +0800

    Update M2Sorting.java

--- seminar2/hw/M2Sorting.java
+++ seminar2/hw/M2Sorting.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class m2_sorting {
+public class M2Sorting {
     private static Logger logger = Logger.getLogger(m2_sorting.class.getName());
 
     public static void main(String[] args) throws SecurityException, IOException {
