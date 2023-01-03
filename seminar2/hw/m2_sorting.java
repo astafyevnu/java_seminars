e95600bd152395e53100911bca635e22ed88b251
Nikolay <79745981+astafyevnu@users.noreply.github.com>
2023-01-03 20:48:01 +0800

    Update m2_sorting.java

--- seminar2/hw/m2_sorting.java
+++ seminar2/hw/m2_sorting.java
@@ -1,19 +1,25 @@
 package seminar2.hw;
 
-import java.io.PrintWriter;
+import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import java.util.Arrays;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class m2_sorting {
     private static Logger logger = Logger.getLogger(m2_sorting.class.getName());
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SecurityException, IOException {
         int arr[] = { -8, 6, -4, 7, 8, 3, 2, -9, 1, 0, -1, -8, 6, -4 };
         int temp = 1;
+        FileHandler fh;
 
-        try (PrintWriter out = new PrintWriter("log.txt")) {
-
+        try {
+            fh = new FileHandler("log.log");  
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
             for (int i = 0; i < arr.length; i++) {
                 for (int j = 1; j < arr.length - i; j++) {
                     if (arr[j - 1] > arr[j]) {
@@ -23,8 +29,8 @@ public class m2_sorting {
                     }
                 }
             }
-            out.println(Arrays.toString(arr));
-            logger.info("Результат записан в файл: log.txt");
+            System.out.println("Результат записан в файл: log.log");
+            logger.info(Arrays.toString(arr));
         } catch (FileNotFoundException e) {
             logger.info(e.getMessage());
         }
